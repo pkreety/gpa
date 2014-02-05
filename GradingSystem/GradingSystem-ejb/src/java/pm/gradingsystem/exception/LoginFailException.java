9c54b4ba5d7730e7402eac9552e3070f78a34e3d
prakriti <prakriti.khanal77@gmail.com>
2014-02-04 22:14:16 -0800

    Grading Sys is removed

--- GradingSystem/GradingSystem-ejb/src/java/pm/gradingsystem/exception/LoginFailException.java
+++
@@ -1,9 +0,0 @@
-package pm.gradingsystem.exception;
-/* @author Tahmasebi */
-public class LoginFailException extends Exception {
-
-    public LoginFailException(String message) {
-        super(message);
-    }
-    
-}
